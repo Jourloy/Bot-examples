b6795cf0009b17c15a3b1f1c3a16be411a13092f
Jourloy <jourloy@yandex.ru>
2020-05-16 00:28:23 +0300

    Update

--- VKbot.py
+++ VKbot.py
@@ -1,142 +1,108 @@
-#Основные библиотеки для работы с ВК
+
+import os
 import vk_api
+import platform
 import requests
+import traceback
 from vk_api.utils import get_random_id
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
-#Другие библиотеки
-import re
-import os
-import sys
-import hues
-import time
-import shutil
-import difflib
-import asyncio
-import traceback
-from os.path import isfile
+import Settings
+import Commands
+
+class Logging():
+    def __init__(self):
+        pass
+
+    def GetText(self):
+        if Settings.LOGGING:
+            print('> Бот получил сообщение')
+
+class Utils():
+    def __init__(self):
+        pass
+
+    def Clear(self):
+        if platform.os == 'Windows':
+            os.system('cls')
+        else:
+            os.system('clear')
 
 class PreStart():
     def __init__(self):
-        self.situation = None
-        self.ADD_TOKEN = None
-        self.path = str(os.path.dirname(os.path.realpath(__file__)))
+        pass
 
-    def preStart(self):
-        try:
-            shutil.copy(self.path+r'\settingsEx.py', self.path+r'\settings.py')
-            self.situation = True
+    def CheckSettings(self):
+        print('Проверка параметров')
 
-        except:
-            hues.error("Создание файла настроек провалено")
-            traceback.print_exc()
-            os.system("pause")
-            self.situation = False
-
-        finally:
-            return self.situation
+        if (Settings.GROUP_TOKEN == None):
+            print('Отсутствует токен группы. Его необходимо вставить в файл настроек. Процесс завершен')
+            exit()
+        if (Settings.ADMIN_ID == None):
+            print('Отсутствует id администратора группы. Его необходимо вставить в файл настроек. Процесс завершен')
+            exit()
 
 class VKbot():
     def __init__(self):
-        self.userID = None
-        self.log = Settings.LOG_ONLINE
-        self.path = str(os.path.dirname(os.path.realpath(__file__)))
-        self.set = settings.Settings()
+        self.log = Logging()
+        self.commands = Commands()
 
-    #Прием сообщений
-    async def getEvent(self):
+    def Connect(self):
+        try:
+            session = requests.Session()
+            vk_session = vk_api.VkApi(token=Settings.GROUP_TOKEN)
+            vk = vk_session.get_api()
+            longpoll = VkLongPoll(vk_session)
+        except Exception:
+            print("""Произошла ошибка при подключении к ВКонтакте.
+
+Что можно попробовать сделать:
+1. Проверьте токен группы в файле настроек. Он должен выглядеть подобно этому:
+   GROUP_TOKEN = "a1s2d3f4g5h6j7k8l9"
+
+2. Проверьте подключение к Интернет. Оно должно быть стабильное и желательно не меньше 1 Мбит/сек
+
+3. Возможно ВКонтакте не отвечает на данный момент. Попробуйте чуть позже
+
+Подробности ошибки:
+----------------------------------------------------
+{}""".format(Exception))
+        else:
+            print("Бот включен и находится в активном состоянии")
+            self.Start(longpoll, vk)
+    
+    def Start(self, longpoll, vk):
+        while 1:
+            information = self.WaitText(longpoll)
+            self.Response(information, vk)
+
+    def WaitText(self, longpoll):
         for event in longpoll.listen():
             if event.type == VkEventType.MESSAGE_NEW and event.text and event.to_me:
-                #Получение текстового сообщения
+                self.log.GetText()
+                return [event.user_id, event.text]
 
-                self.userID = event.user_id
-                userID = event.user_id
+    def Response(self, information, vk):
+        userId = information[0]
+        userText = information[1]
 
-                if event.text == "Привет":
-                    vk.messages.send(
-                        user_id=self.userID,
-                        random_id=get_random_id(),
-                        message="Хеллоу! :)"
-                    )
+        responseText = self.commands.Answer(userText)
 
-                elif event.text == "Мой ID":
-                    vk.messages.send(
-                        user_id=self.userID,
-                        random_id=get_random_id(),
-                        message="Твой ID: " + self.userID
-                    )
+        vk.messages.send(
+            user_id=userId,
+            random_id=get_random_id(),
+            message=responseText
+        )
+    
 
-                elif event.text == "Выключись":
-                    vk.messages.send(
-                        user_id=self.userID,
-                        random_id=get_random_id(),
-                        message="Пока :)"
-                    )
-                    exit()
+if __name__ == "__main__":
+    PreStart = PreStart()
+    VKbot = VKbot()
 
-                else:
-                    vk.messages.send(
-                        user_id=event.user_id,
-                        random_id=get_random_id(),
-                        message="Не понятно, что это за просьба :)"
-                    )
-
-    #Основной модуль бота
-    async def bot(self):
-        while True:
-            await self.getEvent()
-
-    @property
-    def parametrs(self):
-        if Settings.TOKEN == "None":
-            self.ADD_TOKEN = False
-        else:
-            self.ADD_TOKEN = True
-
-        return self.ADD_TOKEN
-
-if __name__ ==  '__main__':
-    path = str(os.path.dirname(os.path.realpath(__file__)))
-    while True:
-        if isfile(path+r'\settings.py'):
-            import settings
-
-            Settings = settings.Settings()
-            VKbot = VKbot()
-            token = VKbot.parametrs
-            if token:
-                session = requests.Session()
-                vk_session = vk_api.VkApi(token=Settings.TOKEN)
-                vk = vk_session.get_api()
-                longpoll = VkLongPoll(vk_session)
-
-                try:
-                    print("\n\n")
-                    loop = asyncio.get_event_loop()
-                    hues.success("Бот включен и находится в активном состоянии")
-                    loop.run_until_complete(VKbot.bot())
-                except (KeyboardInterrupt, SystemExit):
-                    os.system('cls')
-                    hues.log("Бот выключен")
-                    exit()
-            else:
-                os.system("cls")
-                hues.error("Необходимо добавить TOKEN в файл настроек")
-                exit()
-
-        else:
-            try:
-                start = PreStart()
-                situation = start.preStart()
-            except (KeyboardInterrupt, SystemExit):
-                os.system('cls')
-                hues.log("Бот выключен")
-                exit()
-            except Exception as e:
-                print("\n")
-                print("----------ОШИБКА----------")
-                traceback.print_exc()
-                print("--------------------------")
-                exit()
+    PreStart.CheckSettings()
+    try:
+        VKbot.Connect()
+    except Exception:
+        print('\nПроцесс завершен')
