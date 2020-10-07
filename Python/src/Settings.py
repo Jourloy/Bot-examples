deb01afc1dec086f43b85e4a3b089d4d840aa264
Igor Shaposhnikov <jourloy@yandex.ru>
2020-10-07 23:33:43 +0300

    Translate

--- Python/src/Settings.py
+++ Python/src/Settings.py
@@ -1,10 +1,10 @@
-# Сюда необходимо вставить токен группы с правами для отправки сообщений
-# Пример:
+# Here you must insert group token (bot will can send message)
+# For example:
 # GROUP_TOKEN = "a1s2d3f4g5h6j7k8l9"
 GROUP_TOKEN = None
 
-# Необходимо ли выводить в консоль сообщения текущем состоянии бота
-# Пример:
-# > Бот получил запрос
-# > Бот ответил
+# Need logging in console?
+# For example:
+# > Bot get request
+# > Bot answer
 LOGGING = True
