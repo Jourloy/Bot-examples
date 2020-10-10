302b817def008b0d3b375a5c06871bdff3f08ab6
Igor Shaposhnikov <jourloy@yandex.ru>
2020-10-10 20:53:52 +0300

    Move

---
+++ VK/Python/src/Settings.py
@@ -0,0 +1,10 @@
+# Here you must insert group token (bot will can send message)
+# For example:
+# GROUP_TOKEN = "a1s2d3f4g5h6j7k8l9"
+GROUP_TOKEN = None
+
+# Need logging in console?
+# For example:
+# > Bot get request
+# > Bot answer
+LOGGING = True
